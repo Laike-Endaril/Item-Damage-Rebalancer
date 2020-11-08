70c97af25815e35993f3950ed8e6a763f528fce1
Kyle Koder <silvertallon@gmail.com>
2020-11-07 20:04:18 -0800

    1.12.2.003

--- src/main/java/com/fantasticsource/itemdamagerebalancer/ItemDamageRebalancer.java
+++ src/main/java/com/fantasticsource/itemdamagerebalancer/ItemDamageRebalancer.java
@@ -23,7 +23,7 @@ public class ItemDamageRebalancer
 {
     public static final String MODID = "itemdamagerebalancer";
     public static final String NAME = "Item Damage Rebalancer";
-    public static final String VERSION = "1.12.2.002";
+    public static final String VERSION = "1.12.2.003";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
